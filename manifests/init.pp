d5cfeb5eadec0b06c814a0567a21a85ab8e3fb03
Leo Luoto <luoto.leo@gmail.com>
2016-12-11 17:33:06 +0000

    added packages

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,1 +1,12 @@
-test
+class lamp {
+	Package {ensure => "installed", allowcdrom => "true",}
+	package {"apache2":}
+	package {"mysql-client":}
+	package {"mysql-server":}
+	package {"php7.0":}
+	package {"libapache2-mod-php":
+		require => Package ["apache2"],}
+	package {"ekiga":}
+	package {"gedit":}
+	package {"ssh":}
+}
