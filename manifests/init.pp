b1006c8ec3572765f3b73f25528c43045b99e523
Leo Luoto <luoto.leo@gmail.com>
2016-12-11 17:59:26 +0000

    Final version

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,4 +9,36 @@ class lamp {
 	package {"ekiga":}
 	package {"gedit":}
 	package {"ssh":}
-}
+
+	file {"/var/www/html/index.html":
+		content => "Hello World!\n",
+	}
+	
+	file {"/home/xubuntu/public_html/index.php":
+		content => template("lamp/index.php"),
+	}
+	
+	file {"/etc/apache2/mods-enabled/userdir.conf":
+		ensure => "link",
+		target => "/etc/apache2/mods-available/userdir.conf",
+		notify => Service ["apache2"],
+	}
+
+	file {"/etc/apache2/mods-enabled/userdir.load":
+		ensure => "link",
+		target => "/etc/apache2/mods-available/userdir.load",
+		notify => Service ["apache2"],
+	}
+
+	file {"/etc/apache2/mods-available/php7.0.conf":
+		content => template("lamp/php7.0.conf"),
+		notify => Service ["apache2"],
+	}
+
+	service {"apache2":
+		ensure => "running",
+		enable => "true",
+		provider => "systemd",
+	}
+
+
